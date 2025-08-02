e109d40270b06bbb2c29cabc4308c255a50504c6
Aditya Thakur <Aditya@Mac.lan>
2025-08-02 23:14:37 +0530

    Minor Updates

--- src/Homeconnect.jsx
+++ src/Homeconnect.jsx
@@ -23,28 +23,38 @@ function Homeconnect() {
           <div className="conline"></div>
           </div>
           <div className="concontainer">
-            <div className="usleft" data-aos="fade-right">
-                <h2>Get in Touch</h2>
+               <form
+  action="https://formsubmit.co/iconnectgjust@gmail.com"
+  method="POST"
+  className="usleft"
+  data-aos="fade-right"
+>
+  <h2>Get in Touch</h2>
 
-                <div className="frame1">
-                            <div>Full Name</div>
-                            <input type="text" placeholder="Aditya Thakur"></input>
-                        </div>
-                        <div className="frame1">
-                            <div>Email</div>
-                            <input type="text" placeholder="some@example.com"></input>
-                        </div>
-                        <div className="frame1">
-                            <div>Subject</div>
-                            <input type="text" placeholder="Startup spotlight"></input>
-                        </div>
-                        <div className="frame2">
-                            <div>Message</div>
-                            <textarea type="text" placeholder="I want to know about..."></textarea>
-                        </div>
-                        <button className="contbutton">Submit</button>
+  <div className="frame1">
+    <div>Full Name</div>
+    <input type="text" name="name" placeholder="Aditya Thakur" required />
+  </div>
+  <div className="frame1">
+    <div>Email</div>
+    <input type="email" name="email" placeholder="some@example.com" required />
+  </div>
+  <div className="frame1">
+    <div>Subject</div>
+    <input type="text" name="_subject" placeholder="Startup Spotlight" required />
+  </div>
+  <div className="frame2">
+    <div>Message</div>
+    <textarea name="message" placeholder="I want to know about..." required></textarea>
+  </div>
 
-            </div>
+  {/* Hidden input to disable CAPTCHA */}
+  <input type="hidden" name="_captcha" value="false" />
+
+  <button className="contbutton" type="submit">Submit</button>
+</form>
+
+
             <div className="usright" data-aos="fade-left">
                 <div className="ussocials">
                     <h2>Follow us on</h2>
@@ -53,7 +63,7 @@ function Homeconnect() {
           <a href="https://x.com/iconnectgjust?t=GZGkd_h65aoTtg3M5MnmCg&s=08"><i className='bx bxl-twitter'></i></a>
           <a href="https://www.whatsapp.com/channel/0029VaELUPEGU3BNfgEAmU0h"><i className='bx bxl-whatsapp' ></i></a>
           <a href="https://www.linkedin.com/company/iconnect-gjust/"><i className='bx bxl-linkedin' ></i></a>
-          <a href="https://www.youtube.com/@iConnectGJUST"><i class='bx bxl-youtube' ></i></a>
+          <a href="https://www.youtube.com/@iConnectGJUST"><i className='bx bxl-youtube' ></i></a>
         </div>
                 </div>
                 <div className="reachus" data-aos="fade-up">
